c0c3318400e51a86c8c0cff503959762593e5374
matheus-t3ch <matheus.mourat75@gmail.com>
2024-01-20 19:20:06 -0300

    :sparkles: feat - Definindo o Tema do App & Novos Componentes = [ Button , ActivityIndicator, Box ]

--- App.tsx
+++ App.tsx
@@ -6,16 +6,27 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-} from 'react-native';
+import { SafeAreaView, View} from 'react-native';
 import { Text } from './src/components/Text/Text';
+import { ThemeProvider } from '@shopify/restyle';
+import { Button } from './src/components/Button/Button';
+import { theme } from './src/theme/theme';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-     <Text>Olá Nubble</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" italic>
+            Coffstack
+          </Text>
+          <Button title="Primary" marginBottom="s12" />
+          <Button disabled title="Primary" marginBottom="s12" />
+          <Button preset="outline" title="Outline" marginBottom="s12" />
+          <Button disabled preset="outline" title="Lading" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 export default App;
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,26 +1,66 @@
 import React from "react";
-import {
-  Text as RNText,
-  TextProps as RNTextProps,
-  TextStyle,
-} from 'react-native';
+import {TextStyle} from 'react-native';
+import {createText} from '@shopify/restyle';
+import {Theme} from '../../theme/theme';
 
 
-interface TextProps extends RNTextProps {
+const SRText = createText<Theme>();
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+interface TextProps extends SRTextProps {
   preset?: TextVariants;
+  bold?: boolean;
+  italic?: boolean;
+  semiBold?: boolean;
 }
 
 export function Text({
   children,
   preset = 'paragraphMedium',
   style,
-  ...rest
+  bold,
+  italic,
+  semiBold,
+  ...sRTextProps
 }: TextProps) {
+  const fontFamily = getFontFamily(preset, bold, italic, semiBold);
   return (
-    <RNText style={[$fontSizes[preset], style]} {...rest}>
+    <SRText
+      color="backgroundContrast"
+      style={[$fontSizes[preset], {fontFamily}, style]}
+      {...sRTextProps}>
       {children}
-    </RNText>
+    </SRText>
   );
+}
+
+function getFontFamily(
+  preset: TextVariants,
+  bold?: boolean,
+  italic?: boolean,
+  semiBold?: boolean,
+) {
+  if (
+    preset === 'headingLarge' ||
+    preset === 'headingMedium' ||
+    preset === 'headingSmall'
+  ) {
+    return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+  }
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case italic:
+      return $fontFamily.italic;
+    case semiBold && italic:
+      return $fontFamily.mediumItalic;
+    case semiBold:
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
 }
 
 type TextVariants =
@@ -44,4 +84,17 @@ const $fontSizes: Record<TextVariants, TextStyle> = {
 
   paragraphCaption: { fontSize: 12, lineHeight: 16.8 },
   paragraphCaptionSmall: { fontSize: 10, lineHeight: 14 },
+};
+
+export const $fontFamily = {
+  black: 'Satoshi-Black',
+  blackItalic: 'Satoshi-BlackItalic',
+  bold: 'Satoshi-Bold',
+  boldItalic: 'Satoshi-BoldItalic',
+  italic: 'Satoshi-Italic',
+  light: 'Satoshi-Light',
+  lightItalic: 'Satoshi-LightItalic',
+  medium: 'Satoshi-Medium',
+  mediumItalic: 'Satoshi-MediumItalic',
+  regular: 'Satoshi-Regular',
 };
