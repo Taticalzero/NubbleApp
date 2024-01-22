d6ffc91b9cfdaea7545e2bf104ea166e7f9900bf
matheus-t3ch <matheus.mourat75@gmail.com>
2024-01-22 16:20:50 -0300

    :sparkles: feat - Criando o Componente de TextInput + Tela de Login

--- App.tsx
+++ App.tsx
@@ -12,20 +12,38 @@ import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Box} from './src/components/Box/Box';
 import {Icon} from './src/components/Icon/Icon';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" italic>
-            Olá Nuuble
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
           </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <TextInput
+            errorMessage="mensagem de error"
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            RightComponent={<Icon color="gray2" name="eyeOn" />}
+            boxProps={{mb: 's10'}}
+          />
+          <Text color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button marginTop="s48" title="Entrar" />
+          <Button preset="outline" marginTop="s12" title="Criar uma conta" />
         </View>
-        <Box flexDirection="row">
-          <Icon name="eyeOn" color="error" size={40} />
-          <Icon name="eyeOff" color="carrotSecondary" size={50} />
-        </Box>
       </SafeAreaView>
     </ThemeProvider>
   );
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react';
 import {TextStyle} from 'react-native';
 import {createText} from '@shopify/restyle';
 import {Theme} from '../../theme/theme';
-
 
 const SRText = createText<Theme>();
 type SRTextProps = React.ComponentProps<typeof SRText>;
@@ -73,17 +72,17 @@ type TextVariants =
   | 'paragraphCaption'
   | 'paragraphCaptionSmall';
 
-const $fontSizes: Record<TextVariants, TextStyle> = {
-  headingLarge: { fontSize: 32, lineHeight: 38.4 },
-  headingMedium: { fontSize: 22, lineHeight: 26.4 },
-  headingSmall: { fontSize: 18, lineHeight: 23.4 },
+export const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
 
-  paragraphLarge: { fontSize: 18, lineHeight: 25.2 },
-  paragraphMedium: { fontSize: 16, lineHeight: 22.4 },
-  paragraphSmall: { fontSize: 14, lineHeight: 19.6 },
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
 
-  paragraphCaption: { fontSize: 12, lineHeight: 16.8 },
-  paragraphCaptionSmall: { fontSize: 10, lineHeight: 14 },
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
 };
 
 export const $fontFamily = {
