d67a17715804a40094436a73e07be5f241822afc
matheus-t3ch <matheus.mourat75@gmail.com>
2024-01-26 16:32:39 -0300

    :lipstick: TabBar personalizada

--- src/routes/AppTabNavigator.tsx
+++ src/routes/AppTabNavigator.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+BottomTabBarProps,
+createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 
 import {
   FavoriteScreen,
@@ -8,6 +11,8 @@ import {
   MyProfileScreen,
   NewPostScreen,
 } from '@screens';
+
+import { AppTabBar } from './AppTabBar';
 
 
 export type AppTabBottomTabParamList = {
@@ -19,10 +24,17 @@ export type AppTabBottomTabParamList = {
 const Tab = createBottomTabNavigator<AppTabBottomTabParamList>();
 
 export function AppTabNavigator() {
+  function renderTabBar(props: BottomTabBarProps) {
+    return <AppTabBar {...props} />;
+  }
   return (
     <Tab.Navigator
+      tabBar={renderTabBar}
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          paddingTop: 20,
+        },
       }}>
       <Tab.Screen name="HomeScreen" component={HomeScreen} />
       <Tab.Screen name="NewPostScreen" component={NewPostScreen} />
