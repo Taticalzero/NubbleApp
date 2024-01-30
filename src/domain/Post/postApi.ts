1950f60ede24c0bc978146c634f2ba6cc19e0a34
Taticalzero <matheus.moura75@outlook.com>
2024-01-30 12:42:06 -0300

    :construction: Adicionando o Axios ao projeto e refatorando o serviceAPI do Post

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,16 +1,9 @@
-import {PageAPI} from '@api';
+import {PageAPI, api} from '@api';
 
 import {PostAPI} from './postTypes';
 async function getList(): Promise<PageAPI<PostAPI>> {
-  let response = await fetch('http://localhost:3333/user/post', {
-    method: 'GET',
-    headers: {
-      Authorization:
-        'Bearer Mg.R5wy9_TgoQXaBCRrhnJhuOKaDZhvFqFnhSrnI0xYsOMHJfLC6lj45lCaC-7d',
-    },
-  });
-  let data: PageAPI<PostAPI> = await response.json();
-  return data;
+  const response = await api.get<PageAPI<PostAPI>>('user/post');
+  return response.data;
 }
 export const postApi = {
   getList,
