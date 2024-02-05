8795c9fd870908fbe8081f9164f254c7d7581125
Taticalzero <matheus.moura75@outlook.com>
2024-02-05 10:48:43 -0300

    :clock: Adicionando o tempo das postagens

--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-import { Post, PostComment } from '@domain';
+import {PostComment} from '@domain';
 
-import { Box, ProfileAvatar, Text } from '@components';
+import {Box, ProfileAvatar, Text} from '@components';
 
 interface Props {
-  postComment: PostComment
+  postComment: PostComment;
 }
 export function PostCommentItem({postComment}: Props) {
   return (
     <Box flexDirection="row" mb="s16" alignItems="center">
-        <ProfileAvatar imageURL={postComment.author.profileURL} />
-      <Box ml="s12">
-        <Text preset="paragraphSmall" bold>{postComment.author.name}</Text>
-        <Text preset="paragraphSmall" color="gray1">{postComment.message}</Text>
+      <ProfileAvatar imageURL={postComment.author.profileURL} />
+      <Box ml="s12" flex={1}>
+        <Text preset="paragraphSmall" bold>
+          {postComment.author.name}
+        </Text>
+        <Text preset="paragraphSmall" color="gray1">
+          {postComment.message} - {postComment.createdAtRelative}
+        </Text>
       </Box>
     </Box>
   );
